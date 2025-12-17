378ccad5cb0e50beee7e31b600745d888a5f7944
Jen Warman <jenniferwarman@oinspiredthinking.group>
2025-12-17 16:52:43 +0000

    DTP-42 | formatting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,68 @@
-import { useState } from 'react'
-import './App.css'
-import supabase from './utils/supabase'
+import { useState } from "react";
+import "./App.css";
+import supabase from "./utils/supabase";
 
 function App() {
-const [newUser, setNewUser] = useState({
-  username: '',
-  email: '',
-  password: ''
-})
+  const [newUser, setNewUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
 
-async function handleAddNewUser() {
-  event.preventDefault()
+  async function handleAddNewUser() {
+    event.preventDefault();
 
-  const {data, error} = await supabase.auth.signUp({
-    email: newUser.email,
-    password: newUser.password,
-    options: {
-      data: {
-        username: newUser.username
-      }
+    const { data, error } = await supabase.auth.signUp({
+      email: newUser.email,
+      password: newUser.password,
+      options: {
+        data: {
+          username: newUser.username,
+        },
+      },
+    });
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("New User Added", data);
     }
-  })
-  if (error) {
-    console.log(error)
-  } else {
-    console.log('New User Added', data)
   }
-}
 
   return (
-    <form onSubmit={handleAddNewUser}>
-      <label>Name</label>
-        <input type="text" placeholder='name' value={newUser.username} onChange={(event) => setNewUser({...newUser, username: event.target.value})}/>
-      <label>Email</label>
-        <input type="text" placeholder='email' value={newUser.email} onChange={(event) => setNewUser({...newUser, email: event.target.value})}/>
-      <label>Password</label>
-        <input type="text" placeholder='password' value={newUser.password} onChange={(event) => setNewUser({...newUser, password: event.target.value})}/>
-      <button>Submit</button>
-    </form>
-  )
+    <div>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleAddNewUser}>
+        <label>Name</label>
+        <input
+          type="text"
+          placeholder="name"
+          value={newUser.username}
+          onChange={(event) =>
+            setNewUser({ ...newUser, username: event.target.value })
+          }
+        />
+        <label>Email</label>
+        <input
+          type="text"
+          placeholder="email"
+          value={newUser.email}
+          onChange={(event) =>
+            setNewUser({ ...newUser, email: event.target.value })
+          }
+        />
+        <label>Password</label>
+        <input
+          type="text"
+          placeholder="password"
+          value={newUser.password}
+          onChange={(event) =>
+            setNewUser({ ...newUser, password: event.target.value })
+          }
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
