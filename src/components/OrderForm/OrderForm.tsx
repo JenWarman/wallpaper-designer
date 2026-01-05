1c2ad0517765d0c23f7a9bec37ba385ac6ab1f41
Jen Warman <jenniferwarman@oinspiredthinking.group>
2026-01-05 15:31:50 +0000

    DTP-81 | implemented the Form component in the Registerstration and Order form components.

--- src/components/OrderForm/OrderForm.tsx
+++ src/components/OrderForm/OrderForm.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { updateOrderByUserId } from "../../supabase/supabase";
 import styles from "./OrderForm.module.scss";
+import { Form } from "../Form/Form";
+import { Input } from "../Input/Input";
 
 export function OrderForm() {
   const [measurement, setMeasurement] = useState("");
@@ -8,54 +10,57 @@ export function OrderForm() {
   const [height, setHeight] = useState("");
   const [design, setDesign] = useState("DESIGN-1");
 
-  const handleUpdateOrder = async (event: React.FormEvent<HTMLFormElement>) => {
-    event?.preventDefault();
+  const handleUpdateOrder = async () => {
     await updateOrderByUserId(3, 240.79, design);
+  };
+
+  const handleCalculatePrice = (event: React.FormEvent<HTMLFormElement>) => {
+    event?.preventDefault();
+    console.log("calculate price");
   };
 
   return (
     <div className={styles.container}>
       <h4>Your Design: {design}</h4>
-      <form action="" onSubmit={handleUpdateOrder}>
-        <div>
-          <input
-            type="radio"
-            name="cms"
-            id="cms"
-            onChange={() => setMeasurement("cms")}
-          />
-          <label htmlFor="cms">cms</label>
-          <input
-            type="radio"
-            name="inches"
-            id="inches"
-            onChange={() => setMeasurement("inches")}
-          />
-          <label htmlFor="inches">inches</label>
-        </div>
-        <div>
-          <label htmlFor="width">Width of Wall</label>
-          <input
-            type="number"
-            value={width}
-            onChange={(event) => setWidth(event.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="height">Height of Wall</label>
-          <input
-            type="number"
-            value={height}
-            onChange={(event) => setHeight(event.target.value)}
-          />
-        </div>
-        <button>Get Price</button>
-        <div>
-          <p>Our Price: </p>
-          <p>£388.75</p>
-        </div>
-        <button>Order</button>
-      </form>
+      <Form onSubmit={handleCalculatePrice} ctaLabel="Get Price">
+        <Input
+          type="radio"
+          id="cms"
+          value="cms"
+          onChange={() => setMeasurement("cms")}
+          ariaLabel="select cms as measurment"
+          label="cms"
+        />
+        <Input
+          type="radio"
+          id="inches"
+          value="inches"
+          onChange={() => setMeasurement("inches")}
+          ariaLabel="select inches as measurment"
+          label="inches"
+        />
+        <Input
+          type="number"
+          value={width}
+          id="width"
+          onChange={(event) => setWidth(event.target.value)}
+          ariaLabel="width of wall"
+          label="Width of Wall"
+        />
+        <Input
+          type="number"
+          value={height}
+          id="height"
+          onChange={(event) => setHeight(event.target.value)}
+          ariaLabel="height of wall"
+          label="height of Wall"
+        />
+      </Form>
+      <div>
+        <p>Our Price: </p>
+        <p>£388.75</p>
+      </div>
+      <button onClick={handleUpdateOrder}>Order</button>
     </div>
   );
 }
--- src/components/RegisterUser/RegisterUser.tsx
+++ src/components/RegisterUser/RegisterUser.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { signUpAndCreateAccount } from "../../supabase/supabase";
 import type { NewUser } from "../../types/types";
-import styles from "./RegisterUser.module.scss"
+import styles from "./RegisterUser.module.scss";
+import { Form } from "../Form/Form";
+import { Input } from "../Input/Input";
 
 export function RegisterUser() {
   const [newUser, setNewUser] = useState<NewUser>({
@@ -10,56 +12,54 @@ export function RegisterUser() {
     password: "",
   });
 
-  const handleRegisterUser = async (event: React.FormEvent<HTMLFormElement>) => {
+  const handleRegisterUser = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
     event?.preventDefault();
-    const newUserResult = await signUpAndCreateAccount(newUser)
+    const newUserResult = await signUpAndCreateAccount(newUser);
 
     if (!newUserResult.success) {
-        console.log("ERROR: could not create account")
-        return;
+      console.log("ERROR: could not create account");
+      return;
     }
-    console.log("new user registered", newUserResult.user)
+    console.log("new user registered", newUserResult.user);
   };
-  
+
   return (
     <div className={styles.container}>
       <h3>Sign Up</h3>
-      <form onSubmit={handleRegisterUser}>
-        <div>
-        <label>Name</label>
-        <input
+      <Form onSubmit={handleRegisterUser} ctaLabel="Get Price">
+        <Input
+          label="Name"
+          id="name"
+          ariaLabel="name"
           type="text"
           placeholder="name"
           value={newUser.username}
           onChange={(event) =>
-            setNewUser({ ...newUser, username: event.target.value })
-          }
+            setNewUser({ ...newUser, username: event.target.value })}
         />
-        </div>
-        <div>
-        <label>Email</label>
-        <input
+          <Input
+          label="Email"
+          id="email"
+          ariaLabel="email"
           type="text"
           placeholder="email"
           value={newUser.email}
           onChange={(event) =>
-            setNewUser({ ...newUser, email: event.target.value })
-          }
+            setNewUser({ ...newUser, username: event.target.value })}
         />
-        </div>
-        <div>
-        <label>Password</label>
-        <input
-          type="text"
+        <Input
+          label="Password"
+          id="password"
+          ariaLabel="password"
+          type="password"
           placeholder="password"
           value={newUser.password}
           onChange={(event) =>
-            setNewUser({ ...newUser, password: event.target.value })
-          }
+            setNewUser({ ...newUser, username: event.target.value })}
         />
-        </div>
-        <button>Submit</button>
-      </form>
+      </Form>
     </div>
   );
 }
