552d99f95b7acb9b48a67470c29a6042495a937f
Jen Warman <jenniferwarman@oinspiredthinking.group>
2026-01-16 16:50:42 +0000

    DTP-112 | added more conditional classnames.

--- src/components/DesignTile/DesignTile.tsx
+++ src/components/DesignTile/DesignTile.tsx
@@ -10,12 +10,12 @@ export function DesignTile() {
   const designTileClassName = conditionalClassNames({
     [styles.designTile__pink]: formData["background-colour"] === "pink",
     [styles.designTile__blue]: formData["background-colour"] === "blue",
-    [styles.designTile__daisy]: formData.motif === "daisy",
+    // [styles.designTile__daisy]: formData.motif === "daisy",
     [styles.designTile__small]: formData.scale === "small",
     [styles.designTile__medium]: formData.scale === "medium",
     [styles.designTile__large]: formData.scale === "large",
-    [styles.designTile__tile]: formData.repeat === "tile",
-    [styles.designTile__halfDrop]: formData.repeat === "half drop",
+    [styles.designTile__tile]: formData.repeat === "tile" && formData.motif === "daisy",
+    [styles.designTile__halfDrop]: formData.repeat === "half drop" && formData.motif === "daisy",
   });
   return (
     <div className={styles.designTile}>
