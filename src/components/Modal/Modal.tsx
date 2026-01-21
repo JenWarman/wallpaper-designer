06fef79b9661da5391ba1372943553a0cfbdffba
Jen Warman <jenniferwarman@oinspiredthinking.group>
2026-01-21 17:22:22 +0000

    DTP-144 | Modal component created and recieves design data and url as props. The Edit Cta navigates to the design page.

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,47 @@
+import { useNavigate } from "react-router";
+import { dataTestIds } from "../../utils/dataTestIds";
+import { Cta } from "../Cta/Cta";
+import { PatternDesign } from "../PatternDesign/PatternDesign";
+import styles from "./Modal.module.scss";
+
+type ModalProps = {
+  url: string;
+  design: {
+    theme: string;
+    motif: string;
+    scale: string;
+    colour: string;
+    repeat: string;
+  };
+};
+
+export function Modal({ url, design }: ModalProps) {
+ const navigate = useNavigate()
+
+  const handleOrderDesign = () => {
+    console.log("order me!");
+    //navigate to order page with design config
+  };
+  return (
+    <div className={styles.modal__container}>
+      <h1 className={styles.modal__heading}>Your Designs</h1>
+      <PatternDesign  design={design} component="saved"/>
+      <div className={styles.modal__ctas}>
+        <Cta
+          label="Edit"
+          ariaLabel="edit your design"
+          type="button"
+          dataTestId={dataTestIds.cta}
+          ctaFunction={() => navigate(`/design?${url}`)}
+        />
+        <Cta
+          label="Order"
+          ariaLabel="order your design"
+          type="button"
+          dataTestId={dataTestIds.cta}
+          ctaFunction={handleOrderDesign}
+        />
+      </div>
+    </div>
+  );
+}
--- src/components/SavedDesigns/SavedDesigns.tsx
+++ src/components/SavedDesigns/SavedDesigns.tsx
@@ -4,23 +4,36 @@ import { Link } from "react-router";
 import styles from "./SavedDesigns.module.scss";
 import { PatternDesign } from "../PatternDesign/PatternDesign";
 import type { SavedDesign } from "../../types/types";
+import { Modal } from "../Modal/Modal";
 
 export function SavedDesigns() {
-
-  const [designs, setDesigns] = useState<SavedDesign[]>([])
+  const [designs, setDesigns] = useState<SavedDesign[]>([]);
+  const [toggleModal, setToggleModal] = useState(false);
+  const [modalUrl, setModalUrl] = useState("");
+  const [modalData, setModalData] = useState({});
 
   useEffect(() => {
     const fetchDesigns = async () => {
-      const result = await fetchDesignsByUserId()
-      console.log(result)
+      const result = await fetchDesignsByUserId();
+      console.log(result);
 
-      if (!result) return
+      if (!result) return;
 
-      setDesigns(result.data)
-
-    }
-    fetchDesigns()
+      setDesigns(result.data);
+    };
+    fetchDesigns();
   }, []);
+
+  const handleToggleModal = (design_url: string, design_data: {theme: "", motif: "", scale: "", colour: "", repeat: ""}) => {
+    console.log(design_data, "data in toggle");
+    console.log(design_url, "url in toggle");
+    setToggleModal(!toggleModal);
+    setModalUrl(design_url);
+    setModalData(design_data);
+  };
+
+  console.log(modalData);
+  console.log(modalUrl);
 
   return (
     <div className={styles.savedDesigns}>
@@ -34,10 +47,17 @@ export function SavedDesigns() {
           ))}
         </ul> */}
         <div className={styles.savedDesigns__cardContainer}>
-          {designs.map(({design_url, design_data}) => (
-            <PatternDesign key={design_url} design={design_data} component="saved"/>
+          {designs.map(({ design_url, design_data }) => (
+            <div
+              key={design_url}
+              className={styles.savedDesigns__card}
+              onClick={() => handleToggleModal(design_url, design_data)}
+            >
+              <PatternDesign design={design_data} component="saved" />
+            </div>
           ))}
         </div>
+        {toggleModal && <Modal url={modalUrl} design={modalData} />}
       </div>
     </div>
   );
