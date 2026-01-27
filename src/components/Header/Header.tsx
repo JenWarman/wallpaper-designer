c7c39f179068b3d61e77548b3b7f1f6c08bd200e
Jen Warman <jenniferwarman@oinspiredthinking.group>
2026-01-27 12:11:11 +0000

    DTP-155 | Header component is sticky, the Nav component closes when location changes and the test file has been updated accordingly.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BurgerMenu } from "../BurgerMenu/BurgerMenu";
 import styles from "./Header.module.scss";
 import { Nav } from "../Nav/Nav";
 import { dataTestIds } from "../../utils/dataTestIds";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 export function Header() {
   const [showNav, setShowNav] = useState(false);
+  const location = useLocation();
 
   const toggleOpenNav = () => {
     setShowNav(!showNav);
   };
+
+  useEffect(() => {
+    (async () => {
+      setShowNav(false);
+    })();
+  }, [location]);
+
   return (
     <>
-      <div className={styles.header__container} data-testid={dataTestIds.header}>
-        <Link to={"/"} className={styles.header__heading}>Wallpaper Designer</Link>
+      <div
+        className={styles.header__container}
+        data-testid={dataTestIds.header}
+      >
+        <Link to={"/"} className={styles.header__heading}>
+          Wallpaper Designer
+        </Link>
         <div data-testid={dataTestIds.toggleNav} onClick={toggleOpenNav}>
           <BurgerMenu isOpen={showNav} />
         </div>
