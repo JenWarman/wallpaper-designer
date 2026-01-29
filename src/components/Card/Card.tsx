64c1280cf97d9c68d9c133336b5840db8c6255b9
Jen Warman <jenniferwarman@oinspiredthinking.group>
2026-01-29 16:59:30 +0000

    DTP-195 | Card component is rendered inside SavedDesigns component.

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,23 @@
+import type { CardProps} from "../../types/types";
+import { PatternDesign } from "../PatternDesign/PatternDesign";
+import styles from "./Card.module.scss";
+
+
+
+export function Card({ handleClick, design_data, created_at }: CardProps) {
+  return (
+    <div className={styles.card} onClick={handleClick}>
+      <div className={styles.card__pattern}>
+        <PatternDesign design={design_data} component="saved" />
+      </div>
+      <div className={styles.card__text}>
+        <p className={styles.card__name}>
+          {design_data.theme} wallpaper with {design_data.motif} pattern.
+        </p>
+        <p className={styles.card__edit}>
+          Last edited: {new Date(created_at).toLocaleDateString()}
+        </p>
+      </div>
+    </div>
+  );
+}
