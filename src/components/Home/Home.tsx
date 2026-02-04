e92b6494dcf4d20d83780572f5d841bbb60ab0a4
Jen Warman <jenniferwarman@oinspiredthinking.group>
2026-02-04 17:28:41 +0000

    DTP-39 | home screen component.

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router";
+import styles from "./Home.module.scss";
+
+export function Home() {
+  const navigate = useNavigate();
+  return (
+    <div className={styles.home__container}>
+        <h1 className={styles.home__heading}>Design Your Own</h1>
+        <h2 className={styles.home__subHeading}>Wallpaper</h2>
+        <div className={styles.home__ctaContainer}>
+          <button
+            type="button"
+            aria-label="get started"
+            onClick={() => navigate("/register")}
+            className={styles.home__cta}
+          >
+            Get started
+          </button>
+        </div>
+      </div>
+  );
+}
