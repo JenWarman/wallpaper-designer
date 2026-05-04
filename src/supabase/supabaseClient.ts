abd49de68f7fecea64599955a591ef65939b6448
Jen Warman <jen.warman85@gmail.com>
2026-05-04 17:01:46 +0100

    Removed console logs.

--- src/supabase/supabaseClient.ts
+++ src/supabase/supabaseClient.ts
@@ -3,9 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
-console.log("SupabaseUrl:", supabaseUrl)
-console.log("SupabaseAPIKey:", supabaseKey)
-
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase environment variables");
 }
