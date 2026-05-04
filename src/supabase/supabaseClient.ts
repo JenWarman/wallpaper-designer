392ea42143b74bfee61aa33dc0af0e4d5acbe271
Jen Warman <jen.warman85@gmail.com>
2026-05-04 17:01:11 +0100

    supabase api key

--- src/supabase/supabaseClient.ts
+++ src/supabase/supabaseClient.ts
@@ -1,11 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+
+console.log("SupabaseUrl:", supabaseUrl)
+console.log("SupabaseAPIKey:", supabaseKey)
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase environment variables");
 }
+
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
